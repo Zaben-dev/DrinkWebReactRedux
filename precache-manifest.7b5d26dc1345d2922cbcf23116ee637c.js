b167b07a16895380bdd7b4da75e6b8164434118e
Zaben-dev <patrykdmitruk227@gmail.com>
2020-10-31 01:45:40 +0100

    Updates

---
+++ precache-manifest.7b5d26dc1345d2922cbcf23116ee637c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a036c15dd35fd8d92cdc63a0fe7d328f",
+    "url": "/DrinkWebReactRedux/index.html"
+  },
+  {
+    "revision": "092029e41da971a99b6c",
+    "url": "/DrinkWebReactRedux/static/css/main.0bfc1e04.chunk.css"
+  },
+  {
+    "revision": "f00fe5c9fe9a18abed03",
+    "url": "/DrinkWebReactRedux/static/js/2.c4a01364.chunk.js"
+  },
+  {
+    "revision": "174be6077b4d3c839c28fe0a6272ec12",
+    "url": "/DrinkWebReactRedux/static/js/2.c4a01364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092029e41da971a99b6c",
+    "url": "/DrinkWebReactRedux/static/js/main.3a777bdc.chunk.js"
+  },
+  {
+    "revision": "73412c18c2166be34504",
+    "url": "/DrinkWebReactRedux/static/js/runtime-main.b7f8d180.js"
+  }
+]);
